0251086f30514e167b2d726e96d68f5bd4ea2e1f
suyeon-dev <choilynne.dev@gmail.com>
2024-11-16 00:23:44 +0900

    241115-fri: JS part2 선택 과제도 완료~! - 클래스의 생성과 상속

--- 03.javascript/practice-upgrade/practice02.js
+++ 03.javascript/practice-upgrade/practice02.js
@@ -11,3 +11,37 @@ class Shape {
 
 let rec1 = new Shape(3, 4);
 console.log(rec1.getArea()); //12
+
+class Rectangle extends Shape {
+  // 대각선 길이: Math.sqrt() 제곱근 구하는 함수
+  getDiagonal() {
+    return Math.sqrt(this.width ** 2 + this.height ** 2);
+  }
+}
+
+let rec2 = new Rectangle(3, 4);
+console.log(rec2.getArea());
+console.log(rec2.getDiagonal());
+
+class Triangle extends Shape {
+  //넓이
+  getArea() {
+    return (this.width * this.height) / 2;
+  }
+}
+let triangle = new Triangle(3, 4);
+console.log(triangle.getArea());
+
+class Circle extends Shape {
+  //radius 생성자 추가
+  constructor(width, height, radius) {
+    super(width, height);
+    this.radius = radius;
+  }
+  //넓이
+  getArea() {
+    return Math.PI * this.radius ** 2;
+  }
+}
+let circle = new Circle(0, 0, 3);
+console.log(circle.getArea());
