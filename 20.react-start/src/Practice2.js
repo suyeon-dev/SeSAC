8df37d23663f8b6b03eb8b8897aa4563036564f3
suyeon-dev <choilynne.dev@gmail.com>
2025-01-08 00:31:35 +0900

    chore:쓸 모 없는 파일 삭제🔥

--- 20.react-start/src/Practice2.js
+++
@@ -1,4 +0,0 @@
-// 실습2
-export default function Result() {
-  return <div>{3 + 5 == 8 ? '정답입니다!' : '오답입니다!'}</div>;
-}
--- 20.react-start/src/practice3.js
+++
@@ -1,7 +0,0 @@
-// 실습3
-export default function Comparison() {
-  let a = 3;
-  let b = 2;
-
-  return <div>{a > b ? 'a가 b보다 큽니다' : ''}</div>;
-}
